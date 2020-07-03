20845bad2b2ffb22979b6b5939c50f4228000f16
Nikhil Adhau <nikhiladhua999@gmail.com>
2020-07-03 18:34:40 +0530

    adding template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+	{
+		'author' : 'Nikhil',
+		'title' : 'Blog 1',
+		'content' : 'This is my first Blog',
+		'date' : '3 July 2020'
+	},
+	{
+		'author' : 'Alex',
+		'title' : 'Blog 2',
+		'content' : 'This is my second Blog',
+		'date' : '5 July 2020'
+	}
+]
+
 def home (request):
-	return render(request, 'blog/home.html')
+	context = {
+	'posts' : posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about (request):
 	return render(request, 'blog/about.html')
